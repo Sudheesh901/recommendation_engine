8934474e92713dd39b60245484b0598f911acbed
Sudheesh901 <sudheeshkrj@gmail.com>
2026-04-27 14:42:35 +0530

    Integrated Monitoring with Prometheus

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from app.routes import recommendation
 
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
